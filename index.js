d972f2ff8799e77bd686a224746f438e9f163f49
Anurag Dwivedi <anuragd7@gmail.com>
2015-10-24 08:20:10 +0000

    Updated index.js to read from file error corrected

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var fs = require('fs');
-var buf = new Buffer();
+//var buf = new Buffer();
 var app = express();
+var content = fs.readFileSync('index.html','utf-8');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html','utf-8'));
+  response.send(content);
 });
 
 app.listen(app.get('port'), function() {
